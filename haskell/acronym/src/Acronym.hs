f63e0d873d676d1d5211492229299c02a18d2927
summerjacket <dxmcxd@gmail.com>
2019-05-03 21:19:21 -0700

    update acronym

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,6 +1,16 @@
 module Acronym (abbreviate) where
 
-abbreviate :: String -> String
-abbreviate xs = undefined
+import Data.Char (toUpper, isUpper)
+
+shorten :: String -> String
+shorten [] = []
+shorten word
+    | all isUpper word = [head word]
+    | '-' `elem` word = shorten left ++ shorten right
+    | otherwise = toUpper (head word) : filter isUpper (tail word)
     where
-        delimiters = [' ', ',', '-']
+        left = takeWhile (/='-') word
+        right = tail $ dropWhile (/='-') word
+
+abbreviate :: String -> String
+abbreviate phrase = concat $ map shorten $ words phrase
