f6f17491244fbd9a6c637fb7cef13009fd429c59
summerjacket <dxmcxd@gmail.com>
2019-05-14 13:08:35 -0700

    haskell: update saddle point

--- haskell/saddle-points/src/Matrix.hs
+++ haskell/saddle-points/src/Matrix.hs
@@ -1,14 +1,16 @@
 module Matrix (saddlePoints) where
 
-import Data.Array (Array, Ix, bounds, (!), range)
+import Data.Array (Array, bounds, (!), range)
 
-isSaddlePoint :: (Ix r, Ix c, Num r, Num c, Enum r, Enum c, Ord e) => Array (r, c) e -> (r, c) -> (r, c) -> Bool
+type Point = (Int, Int)
+
+isSaddlePoint :: Ord e => Array Point e -> Point -> Point -> Bool
 isSaddlePoint matrix (rows, cols) point = rowTest && colTest
   where
     rowTest = all (\r -> (matrix ! (r, snd point)) >= valAtPoint) [0..rows]
     colTest = all (\c -> (matrix ! (fst point, c)) <= valAtPoint) [0..cols]
     valAtPoint = matrix ! point
 
-saddlePoints :: (Ix r, Ix c, Num r, Num c, Enum r, Enum c, Ord e) => Array (r, c) e -> [(r, c)]
+saddlePoints :: Ord e => Array Point e -> [Point]
 saddlePoints matrix = filter (isSaddlePoint matrix size) (range $ bounds matrix)
   where (_, size) = bounds matrix
