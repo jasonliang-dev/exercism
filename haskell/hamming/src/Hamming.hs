d4ffc72fa3ef2cf2c48ccd9fe48c76e58311ec26
summerjacket <dxmcxd@gmail.com>
2019-05-13 22:01:19 -0700

    haskell: hamming

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,6 +1,8 @@
 module Hamming (distance) where
 
+import Data.Function (on)
+
 distance :: String -> String -> Maybe Int
 distance xs ys
-    | length xs /= length ys = Nothing
+    | ((/=) `on` length) xs ys = Nothing
     | otherwise = Just $ length $ filter not $ zipWith (==) xs ys
