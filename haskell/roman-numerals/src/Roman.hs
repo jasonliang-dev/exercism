129899d7b06cfa61e03f42e03fc93480dc1bf08b
summerjacket <dxmcxd@gmail.com>
2019-05-18 00:05:18 -0700

    haskell: updated roman

--- haskell/roman-numerals/src/Roman.hs
+++ haskell/roman-numerals/src/Roman.hs
@@ -5,24 +5,24 @@ where
 
 import Data.List ( find )
 
-symbols :: [(Integer, String)]
-symbols =
-  [ (1000, "M")
-  , (900 , "CM")
-  , (500 , "D")
-  , (400 , "CD")
-  , (100 , "C")
-  , (90  , "XC")
-  , (50  , "L")
-  , (40  , "XL")
-  , (10  , "X")
-  , (9   , "IX")
-  , (5   , "V")
-  , (4   , "IV")
-  , (1   , "I")
-  ]
-
 numerals :: Integer -> Maybe String
 numerals 0 = Just ""
-numerals n = (snd <$> pair) <> (((n -) . fst <$> pair) >>= numerals)
-  where pair = find ((<= n) . fst) symbols
+numerals n = do
+  (base, symbol) <- find ((<= n) . fst) symbols
+  (symbol ++) <$> numerals (n - base)
+ where
+  symbols =
+    [ (1000, "M")
+    , (900 , "CM")
+    , (500 , "D")
+    , (400 , "CD")
+    , (100 , "C")
+    , (90  , "XC")
+    , (50  , "L")
+    , (40  , "XL")
+    , (10  , "X")
+    , (9   , "IX")
+    , (5   , "V")
+    , (4   , "IV")
+    , (1   , "I")
+    ]
