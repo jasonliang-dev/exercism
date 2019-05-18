765449c72674a5760d7ce55f18bdfd57ddc21c24
summerjacket <dxmcxd@gmail.com>
2019-05-18 01:04:34 -0700

    clojure: update series

--- clojure/series/src/series.clj
+++ clojure/series/src/series.clj
@@ -1,10 +1,22 @@
 (ns series)
 
-(defn slices
+(defn slices-old
   [string length]
   (cond
     (zero? length) [""]
     (< (count string) length) []
     :else (cons
            (apply str (take length string))
-           (slices (drop 1 string) length))))
+           (slices-old (drop 1 string) length))))
+
+(defn slices
+  [string length]
+  (if (zero? length)
+    [""]
+    (loop [s string
+           acc []]
+      (if (< (count s) length)
+        acc
+        (recur
+         (drop 1 s)
+         (conj acc (apply str (take length s))))))))
