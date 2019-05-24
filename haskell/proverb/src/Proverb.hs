acc3aa0ebdbf028a5c52d2c70df4d82f5abe4eb4
summerjacket <dxmcxd@gmail.com>
2019-05-24 00:18:26 -0700

    haskell: update proverb

--- haskell/proverb/src/Proverb.hs
+++ haskell/proverb/src/Proverb.hs
@@ -3,11 +3,10 @@ module Proverb
   )
 where
 
-line :: String -> String -> String
-line current next =
-  "For want of a " ++ current ++ " the " ++ next ++ " was lost."
-
 recite :: [String] -> String
 recite [] = ""
 recite (x : xs) =
   unlines (zipWith line (x : xs) xs) ++ "And all for the want of a " ++ x ++ "."
+ where
+  line current next =
+    "For want of a " ++ current ++ " the " ++ next ++ " was lost."
