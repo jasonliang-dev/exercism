8e14ce02993ff4cf202c0535ea5855330b2d499e
summerjacket <dxmcxd@gmail.com>
2019-05-28 20:34:41 -0700

    haskell: update ocr numbers

--- haskell/ocr-numbers/src/OCR.hs
+++ haskell/ocr-numbers/src/OCR.hs
@@ -1,28 +1,23 @@
 module OCR (convert) where
 
-import Data.List (lookup, intercalate)
+import Data.List (lookup, intercalate, transpose)
 import Data.List.Split (chunksOf)
 import Data.Maybe (fromMaybe)
 
 convertLine :: [String] -> String
-convertLine xs
-  | all null xs = []
-  | otherwise   = fromMaybe '?' (lookup ch numbers) : convertLine rest
+convertLine xs | all null xs = []
+               | otherwise = fromMaybe '?' (lookup ch table) : convertLine rest
  where
-  ch   = map (take 3) xs
-  rest = map (drop 3) xs
-  numbers =
-    [ ([" _ ", "| |", "|_|", "   "], '0')
-    , (["   ", "  |", "  |", "   "], '1')
-    , ([" _ ", " _|", "|_ ", "   "], '2')
-    , ([" _ ", " _|", " _|", "   "], '3')
-    , (["   ", "|_|", "  |", "   "], '4')
-    , ([" _ ", "|_ ", " _|", "   "], '5')
-    , ([" _ ", "|_ ", "|_|", "   "], '6')
-    , ([" _ ", "  |", "  |", "   "], '7')
-    , ([" _ ", "|_|", "|_|", "   "], '8')
-    , ([" _ ", "|_|", " _|", "   "], '9')
+  ch      = map (take 3) xs
+  rest    = map (drop 3) xs
+  numbers = transpose $ map
+    (chunksOf 3)
+    [ " _     _  _     _  _  _  _  _ "
+    , "| |  | _| _||_||_ |_   ||_||_|"
+    , "|_|  ||_  _|  | _||_|  ||_| _|"
+    , "                              "
     ]
+  table = zip numbers ['0' .. '9']
 
 convert :: String -> String
 convert = intercalate "," . map convertLine . chunksOf 4 . lines
