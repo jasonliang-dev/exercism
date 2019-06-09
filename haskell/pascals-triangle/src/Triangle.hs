56d3af7b8c83445eea19115d822cb0e653b91d6a
summerjacket <jasonliang512@gmail.com>
2019-06-09 12:25:11 -0700

    haskell: pascal's triangle

---
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -0,0 +1,15 @@
+module Triangle
+  ( rows
+  )
+where
+
+row :: [Integer] -> [Integer]
+row xs = 1 : snd (foldr buildRow (0, []) xs)
+  where buildRow curr (prev, acc) = (curr, prev + curr : acc)
+
+rows :: Int -> [[Integer]]
+rows = reverse . go
+ where
+  go 0 = []
+  go 1 = [[1]]
+  go x = row (head rest) : rest where rest = go (x - 1)
