821a56ae803ff1581da008840e4d4fea7909f7c6
Jason Liang <jasonliang512@gmail.com>
2019-09-22 18:24:56 -0700

    elisp: hello world

---
+++ elisp/hello-world/hello-world.el
@@ -0,0 +1,12 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun hello ()
+  "Return 'Hello, World!'."
+  "Hello, World!")
+
+(provide 'hello-world)
+;;; hello-world.el ends here
